b898201bc491cd8aac57c885c1909161bfff86ff
Masoud <mmtaee642gmail.com>
2020-08-16 14:12:04 +0430

    add robots.txt

--- pylinux/urls.py
+++ pylinux/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from decorator_include import decorator_include
 from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
 
 
 from .decorators import *
@@ -23,10 +24,12 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
+    path('robots.txt/', TemplateView.as_view(template_name='robots.txt', content_type="text/plain")),
 ]
 
 urlpatterns += i18n_patterns(
     path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    # TODO : change secret in deployment
     path('secret/', admin.site.urls),
     path('main/', decorator_include(writeruser_required, 'base.urls')),
     path('', decorator_include(complete_oauth_profile, 'blog.urls')),
